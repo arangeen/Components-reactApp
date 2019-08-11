cb23b0666260a6de1858e65c1b606659ee1b25f3
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-08-11 16:52:41 -0700

    specifying images in JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avater">
-          <img alt="avater" />
+          <img alt="avater" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
