4ada08b44cf23659747b1e7b3aa8756829217b12
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-08-12 17:27:53 -0700

    showing custom children components and reusing components

--- src/index.js
+++ src/index.js
@@ -7,25 +7,34 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        comment="Hey there!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 6:45AM"
-        comment="How are you doing. I love your post!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Marcus"
-        timeAgo="Today at 2:45PM"
-        comment="D Wade is the best Basketball player!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>Are you sure you want to do this?</ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          comment="Hey there!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 6:45AM"
+          comment="How are you doing. I love your post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Marcus"
+          timeAgo="Today at 2:45PM"
+          comment="D Wade is the best Basketball player!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
